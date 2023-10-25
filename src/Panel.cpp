a4bbbcdb645935f0827a5bc45ff07a38f45edcae
Peter Wilhelmsson <2hdddg@gmail.com>
2023-10-25 18:42:08 +0200

    Maybe correct color parsing

--- src/Panel.cpp
+++ src/Panel.cpp
@@ -14,11 +14,59 @@ struct RGBA {
         const sol::optional<std::string> optionalColor = t[name];
         return optionalColor ? RGBA::FromString(*optionalColor) : RGBA{};
     }
-    static RGBA FromString(const std::string& s) { return RGBA{.g = 1, .a = 1}; }
+    static RGBA FromString(const std::string& s) {
+        // Should be on format #rrggbb or #rrggbbaa
+        if (s.length() != 7 && s.length() != 9) {
+            return RGBA{};
+        }
+        if (s[0] != '#') {
+            return RGBA{};
+        }
+        RGBA c{};
+        bool ok =
+            FromChars(s[1], s[2], c.r) && FromChars(s[3], s[4], c.g) && FromChars(s[5], s[6], c.b);
+        if (!ok) {
+            return RGBA{};
+        }
+        if (s.length() == 7) {
+            c.a = 1;
+            return c;
+        }
+        if (!FromChars(s[7], s[8], c.a)) {
+            return RGBA{};
+        }
+        return c;
+    }
     double r;
     double g;
     double b;
     double a;
+
+   private:
+    static bool FromChar(const char c, uint8_t& n) {
+        if (c >= '0' && c <= '9') {
+            n = c - '0';
+            return true;
+        }
+        if (c >= 'a' && c <= 'f') {
+            n = c - 'a' + 10;
+            return true;
+        }
+        if (c >= 'A' && c <= 'F') {
+            n = c - 'a' + 10;
+            return true;
+        }
+        return false;
+    }
+    static bool FromChars(const char c1, const char c2, double& c) {
+        uint8_t n1 = 0, n2 = 0;
+        if (!FromChar(c1, n1) || !FromChar(c2, n2)) {
+            return false;
+        }
+        uint8_t n = n1 | (n2 << 4);
+        c = n / 255.0;
+        return true;
+    }
 };
 
 static int GetIntProperty(const sol::table& t, const char* name, int missing) {
