f8c46ae169a8b8d568a6913df89b1a16e300a699
Peter Wilhelmsson <2hdddg@gmail.com>
2023-11-10 19:18:40 +0100

    Clean up config

--- config.lua
+++ config.lua
@@ -25,257 +25,181 @@ local function get_icon_for_audio_type()
   return ""
 end
 
-zen.buffers = {
-  num = 1,
-  width = 1000,
-  height = 1000,
-}
-
-zen.panels.left = {
-  widgets = {
-    {
-      sources = {'workspace'},
-      padding = {left = 10 },
-      render = function(displayName)
-        local display = zen.sources.displays[displayName]
-        if not display then
-          return ""
-        end
-        local workspaces = {}
-        for _, workspace in pairs(display.workspaces) do
-          local wmarkup = "<span size='20pt' color='" .. COLOR_BLACK .. "'>" .. workspace.name .. "</span>"
-          local amarkup = ""
-          for _, app in pairs(workspace.applications) do
-            if app.focus or (amarkup == "" and app.next) then
-              amarkup = "<span size='20pt' color='" .. COLOR_BLACK .. "'>" .. app.name .. "</span>"
-            end
-          end
-          boxcolor = COLOR_BLACK_BR
-          if workspace.focus then
-            boxcolor = COLOR_GREEN
-          end
-          local workspace = {
-            type = "flex",
-            direction = "row",
-            padding = {
-              right = 1,
-            },
-            items = {
-              {
-                type = "box",
-                markup = wmarkup,
-                color = boxcolor,
-                padding = {
-                  top = 3,
-                  left = 12,
-                  right = 12,
-                  bottom = 5,
-                },
-                radius = 15,
-                border = {
-                  width = 2,
-                  color = boxcolor .. '10',
-                },
-              },
-              {
-                type = "box",
-                markup = amarkup,
-                color = boxcolor,
-                padding = {
-                  top = 3,
-                  left = 10,
-                  right = 10,
-                  bottom = 5,
-                },
-                radius = 15,
-                border = {
-                  width = 2,
-                  color = boxcolor .. '10',
-                },
-              },
-            },
-          }
-          table.insert(workspaces, workspace)
-        end
-        return {
-          type = "flex",
-          direction = "column",
-          padding = {
-            left = 10,
-            bottom = 1,
-          },
-          items = workspaces,
-        }
-      end,
+local function get_box(markup, color)
+  return {
+    type = "box",
+    markup = markup,
+    color = color,
+    padding = {
+      top = 3,
+      left = 10,
+      right = 10,
+      bottom = 5,
     },
+    radius = 15,
+    border = {
+      width = 2,
+      color = color .. '10',
+    },
+  }
+end
+
+local function render_workspaces(displayName)
+  local display = zen.sources.displays[displayName]
+  if not display then
+    return ""
+  end
+  local workspaces = {}
+  for _, workspace in pairs(display.workspaces) do
+    local wmarkup = "<span size='20pt' color='" .. COLOR_BLACK .. "'>" .. workspace.name .. "</span>"
+    local amarkup = ""
+    for _, app in pairs(workspace.applications) do
+      if app.focus or (amarkup == "" and app.next) then
+        amarkup = "<span size='20pt' color='" .. COLOR_BLACK .. "'>" .. app.name .. "</span>"
+      end
+    end
+    boxcolor = COLOR_BLACK_BR
+    if workspace.focus then
+      boxcolor = COLOR_GREEN
+    end
+    local workspace = {
+      type = "flex",
+      direction = "row",
+      padding = { right = 1 },
+      items = {
+        get_box(wmarkup, boxcolor),
+        get_box(amarkup, boxcolor),
+      },
+    }
+    table.insert(workspaces, workspace)
+  end
+  return {
+    type = "flex",
+    direction = "column",
+    padding = { left = 10, bottom = 1 },
+    items = workspaces,
+  }
+end
+
+local function render_time()
+  local t = os.time()
+  local markup = os.date(
+  "<span font='digital-7' size='40pt' color='#1c1b19' rise='-3pt'>%H:%M</span><span size='15pt' color='#1c1b19'>\n%Y-%m-%d</span>")
+  return get_box(markup, COLOR_BLUE_BR)
+end
+
+local function render_keyboard()
+  icon = ""
+  text = zen.sources.keyboard.layout
+  markup = "<span size='30pt' color='" .. COLOR_BLACK .. "'>" .. icon .. "</span>" ..
+  "<span size='12pt' rise='8pt' color='" .. COLOR_BLACK .. "'> " .. text .. "</span>"
+  return get_box(markup, COLOR_CYAN)
+end
+
+local function render_audio()
+  local markup = ""
+  local icon = ""
+  local icon2 = ""
+  local text = ""
+  color = COLOR_GREEN
+  if zen.sources.audio.muted then
+    icon = icon .. ""
+    text = "Muted"
+    color = COLOR_RED
+  else
+    icon = get_icon_for_audio_type()
+    local volume = math.floor(zen.sources.audio.volume)
+    icon2 = ""
+    if volume > 10 then
+      if volume < 50 then
+        icon2 = ""
+      else
+        icon2 = ""
+      end
+    end
+    text = "Volume " .. volume
+  end
+  markup =
+    "<span size='30pt' color='" .. COLOR_BLACK .. "'>" .. icon .. "</span>" ..
+    "<span size='15pt'> </span>" ..
+    "<span size='30pt' color='" .. COLOR_BLACK .. "'>" .. icon2 .. "</span>" ..
+    "<span size='15pt' rise='8pt' color='" .. COLOR_BLACK .. "'> " .. text .. "</span>"
+  return get_box(markup, color)
+end
+
+local function render_power()
+  local color = COLOR_GREEN
+  local icon = ""
+  local text = ""
+  if zen.sources.power.isCharging then
+    icon = ""
+    text = "Charging"
+  elseif zen.sources.power.isPluggedIn then
+    icon = ""
+    text = "Fully charged"
+  else
+    local c = zen.sources.power.capacity
+    if c > 90 then
+      icon = ""
+    elseif c > 80 then
+      icon = ""
+    elseif c > 50 then
+      icon = ""
+      color = COLOR_YELLOW
+    elseif c > 20 then
+      icon = ""
+      color = COLOR_YELLOW
+    else
+      icon = ""
+      color = COLOR_RED
+    end
+    text = "Battery " .. c .. "%"
+  end
+  local markup = "<span size='30pt' color='" .. COLOR_BLACK .. "'>" .. icon .. "</span>" ..
+  "<span size='15pt' rise='8pt' color='" .. COLOR_BLACK .. "'> " .. text .. "</span>"
+  return get_box(markup, color)
+end
+
+zen = {
+  buffers = {
+    num = 1,
+    width = 1000,
+    height = 1000,
   },
-}
-
-zen.panels.right = {
-  screen_border_offset = 50,
-  widgets = {
-    {
-      sources = {'time', 'date'},
-      padding = {
-        bottom = 10,
-        right = 10,
+  sources = {},
+  panels = {
+    left = {
+      widgets = {
+        {
+          sources = {'workspace'},
+          padding = {left = 10 },
+          render = render_workspaces,
+        },
       },
-      render = function()
-        local t = os.time()
-        local markup = os.date(
-        "<span font='digital-7' size='40pt' color='#1c1b19' rise='-3pt'>%H:%M</span><span size='15pt' color='#1c1b19'>\n%Y-%m-%d</span>")
-        return {
-          type = "box",
-          markup = markup,
-          color = COLOR_BLUE_BR,
-          radius = 15,
-          border = {
-            width = 2,
-            color = COLOR_BLUE_BR .. '80',
-          },
-          padding = {
-            left = 10,
-            right = 10,
-            top = 5,
-            bottom = 5,
-          },
-        }
-      end,
     },
-    {
-      sources = {'keyboard'},
-      padding = {
-        bottom = 10,
-        right = 10,
+    right = {
+      widgets = {
+        {
+          sources = {'time', 'date'},
+          padding = { bottom = 10, right = 10 },
+          render = render_time,
+        },
+        {
+          sources = {'keyboard'},
+          padding = { bottom = 10, right = 10 },
+          render = render_keyboard,
+        },
+        {
+          sources = {'audio'},
+          padding = { bottom = 10, right = 10 },
+          render = render_audio,
+        },
+        {
+          sources = {'power'},
+          padding = { right = 10 },
+          render = render_power,
+        },
       },
-      render = function()
-        icon = ""
-        text = zen.sources.keyboard.layout
-        color = COLOR_CYAN
-        markup = "<span size='30pt' color='" .. COLOR_BLACK .. "'>" .. icon .. "</span>" ..
-        "<span size='12pt' rise='8pt' color='" .. COLOR_BLACK .. "'> " .. text .. "</span>"
-        return {
-          type = "box",
-          markup = markup,
-          color = color,
-          radius = 15,
-          border = {
-            width = 2,
-            color = color .. '80',
-          },
-          padding = {
-            left = 8,
-            right = 8,
-            top = 3,
-            bottom = 3,
-          },
-        }
-      end,
-    },
-    {
-      sources = {'audio'},
-      padding = {
-        bottom = 10,
-        right = 10,
-      },
-      render = function()
-        local markup = ""
-        local icon = ""
-        local icon2 = ""
-        local text = ""
-        color = COLOR_GREEN
-        if zen.sources.audio.muted then
-          icon = icon .. ""
-          text = "Muted"
-          color = COLOR_RED
-        else
-          icon = get_icon_for_audio_type()
-          local volume = math.floor(zen.sources.audio.volume)
-          icon2 = ""
-          if volume > 10 then
-            if volume < 50 then
-              icon2 = ""
-            else
-              icon2 = ""
-            end
-          end
-          text = "Volume " .. volume
-        end
-        markup =
-          "<span size='30pt' color='" .. COLOR_BLACK .. "'>" .. icon .. "</span>" ..
-          "<span size='15pt'> </span>" ..
-          "<span size='30pt' color='" .. COLOR_BLACK .. "'>" .. icon2 .. "</span>" ..
-          "<span size='15pt' rise='8pt' color='" .. COLOR_BLACK .. "'> " .. text .. "</span>"
-        return {
-          type = "box",
-          markup = markup,
-          color = color,
-          radius = 15,
-          border = {
-            width = 2,
-            color = color .. '80',
-          },
-          padding = {
-            left = 10,
-            right = 10,
-            top = 5,
-            bottom = 5,
-          },
-        }
-      end,
-    },
-    {
-      sources = {'power'},
-      padding = {
-        right = 10,
-      },
-      render = function()
-        local color = COLOR_GREEN
-        local icon = ""
-        local text = ""
-        if zen.sources.power.isCharging then
-          icon = ""
-          text = "Charging"
-        elseif zen.sources.power.isPluggedIn then
-          icon = ""
-          text = "Fully charged"
-        else
-          local c = zen.sources.power.capacity
-          if c > 90 then
-            icon = ""
-          elseif c > 80 then
-            icon = ""
-          elseif c > 50 then
-            icon = ""
-            color = COLOR_YELLOW
-          elseif c > 20 then
-            icon = ""
-            color = COLOR_YELLOW
-          else
-            icon = ""
-            color = COLOR_RED
-          end
-          text = "Battery " .. c .. "%"
-        end
-        local markup = "<span size='30pt' color='" .. COLOR_BLACK .. "'>" .. icon .. "</span>" ..
-        "<span size='15pt' rise='8pt' color='" .. COLOR_BLACK .. "'> " .. text .. "</span>"
-        return {
-          type = "box",
-          markup = markup,
-          color = color,
-          radius = 15,
-          border = {
-            width = 2,
-            color = color .. '80',
-          },
-          padding = {
-            left = 10,
-            right = 10,
-            top = 5,
-            bottom = 5,
-          },
-        }
-      end,
     },
   },
 }
