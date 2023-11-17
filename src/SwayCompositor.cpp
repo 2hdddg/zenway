ba2604e9bf1761021ce396c16c45f7eecbfbdd30
Peter Wilhelmsson <2hdddg@gmail.com>
2023-11-17 18:53:34 +0100

    Reduced number of sway files

--- src/SwayCompositor.cpp
+++ src/SwayCompositor.cpp
@@ -10,10 +10,120 @@
 
 #include <iostream>
 #include <map>
+#include <nlohmann/json.hpp>
 #include <set>
 
-#include "SwayJson.h"
+using namespace nlohmann;
 
+const std::set<std::string> ignore = {"rect",
+                                      "window_rect",
+                                      "deco_rect",
+                                      "geometry",
+                                      "border",
+                                      "layout",
+                                      "current_border_width",
+                                      "orientation",
+                                      "percent",
+                                      "sticky",
+                                      "window_properties",
+                                      "idle_inhibitors",
+                                      "representation",
+                                      "transform",
+                                      "scale",
+                                      "serial",
+                                      "modes",
+                                      "current_mode"};
+
+const auto filter = [](int depth, json::parse_event_t event, json &parsed) {
+    if (event != json::parse_event_t::key) return true;
+    std::string key = parsed;
+    return ignore.find(key) == ignore.end();
+};
+
+static void ParseEvent(const std::string &payload) {
+    auto rootNode = json::parse(payload, filter, false /*ignore exceptions*/);
+    std::string change = rootNode["change"];
+    spdlog::trace("Change is: {}", change);
+}
+
+static void ParseBarStateUpdateEvent(const std::string &payload, bool &visibleByModifier) {
+    auto rootNode = json::parse(payload, filter, false /*ignore exceptions*/);
+    // std::string barId = rootNode["id"];
+    visibleByModifier = rootNode["visible_by_modifier"];
+}
+
+static void ParseApplication(Workspace &workspace, nlohmann::basic_json<> applicationNode,
+                             int nextFocusId) {
+    if (applicationNode["type"] != "con" && applicationNode != "floating_con") {
+        spdlog::error("Expected app");
+        return;
+    }
+    auto applicationname = applicationNode["name"];
+    // When name is null and there is an orientation different
+    // from none there are more nodes below! Maybe name is not the correct
+    // check...
+    if (applicationname.is_null()) {
+        auto applicationNodes = applicationNode["nodes"];
+        for (auto &innerApplicationNode : applicationNodes) {
+            ParseApplication(workspace, innerApplicationNode, nextFocusId);
+        }
+        return;
+    }
+
+    auto application = Application{.name = applicationNode["name"]};
+    int applicationId = applicationNode["id"];
+    application.appId = applicationNode["app_id"];
+    // In sway the only focused app is the one in the focused workspace. When that app is focused
+    // the workspace is not. To simplify usage this considers the focused app in a non focused
+    // workspace to be the next in line.
+    application.isFocused = applicationNode["focused"];
+    if (application.isFocused) {
+        workspace.isFocused = true;
+    } else {
+        application.isFocused = applicationId == nextFocusId;
+    }
+    workspace.applications.push_back(std::move(application));
+}
+
+static void ParseTree(const std::string &payload, std::shared_ptr<ScriptContext> scriptContext) {
+    auto rootNode = json::parse(payload, filter, false /*ignore exceptions*/);
+    if (rootNode["type"] != "root") {
+        spdlog::error("Expected root");
+        return;
+    }
+    Displays displays;
+    auto outputNodes = rootNode["nodes"];
+    for (auto outputNode : outputNodes) {
+        if (outputNode["type"] != "output") {
+            // spdlog::error("Expected output but was {}", outputNode["type"]);
+            continue;
+        }
+        auto display = Display{.name = outputNode["name"]};  // workspaces->AddDisplay();
+        auto workspaceNodes = outputNode["nodes"];
+        for (auto workspaceNode : workspaceNodes) {
+            if (workspaceNode["type"] != "workspace") {
+                spdlog::error("Expected workspace");
+                continue;
+            }
+            auto workspace = Workspace{.name = workspaceNode["name"]};
+            //   Better way?
+            auto focusNode = workspaceNode["focus"];
+            int nextFocusId = -1;
+            for (auto i : focusNode) {
+                nextFocusId = i;
+                break;
+            }
+            workspace.isFocused = workspaceNode["focused"];
+            auto applicationNodes = workspaceNode["nodes"];
+            for (auto applicationNode : applicationNodes) {
+                ParseApplication(workspace, applicationNode, nextFocusId);
+            }
+            display.workspaces.push_back(std::move(workspace));
+        }
+        displays.push_back(std::move(display));
+    }
+    scriptContext->Publish(displays);
+}
 static constexpr auto MAGIC = "i3-ipc";
 static constexpr auto MAGIC_LENGTH = 6;
 static constexpr auto HEADER_SIZE = MAGIC_LENGTH + 4 + 4;
@@ -102,7 +212,7 @@ void SwayCompositor::OnRead() {
     switch (msg) {
         case Message::GET_TREE:
             spdlog::trace("Received sway tree");
-            SwayJson::ParseTree(m_payload, m_scriptContext);
+            ParseTree(m_payload, m_scriptContext);
             m_manager->DirtyWorkspace();
             break;
         case Message::SUBSCRIBE:
@@ -110,17 +220,17 @@ void SwayCompositor::OnRead() {
             break;
         case Message::EVENT_WORKSPACE:
             spdlog::trace("Workspace event");
-            SwayJson::ParseEvent(m_payload);
+            ParseEvent(m_payload);
             Send(m_fd, Message::GET_TREE, "");
             break;
         case Message::EVENT_WINDOW:
             spdlog::trace("Window event");
-            SwayJson::ParseEvent(m_payload);
+            ParseEvent(m_payload);
             Send(m_fd, Message::GET_TREE, "");
             break;
         case Message::EVENT_BAR_STATE_UPDATE:
             bool visible;
-            SwayJson::ParseBarStateUpdateEvent(m_payload, visible);
+            ParseBarStateUpdateEvent(m_payload, visible);
             spdlog::debug("Bar update, visible: {}", visible);
             if (visible) {
                 m_manager->Show();
@@ -130,7 +240,7 @@ void SwayCompositor::OnRead() {
             break;
         case Message::EVENT_SHUTDOWN:
             spdlog::trace("Shutdown event");
-            SwayJson::ParseEvent(m_payload);
+            ParseEvent(m_payload);
             break;
         default:
             spdlog::error("Received unhandled sway message: {}", (uint32_t)msg);
